ad88f87ff995fd865582293b0ff17e94540a4838
Макс <@RYZEN-5-3600>
2022-06-07 23:18:41 +0300

    Добавьте файлы проекта.

---
+++ ShutDownComputer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ShutDownComputer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnExec = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.infoButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExec
+            // 
+            this.btnExec.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExec.Location = new System.Drawing.Point(20, 31);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(235, 23);
+            this.btnExec.TabIndex = 0;
+            this.btnExec.Text = "Старт";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.Location = new System.Drawing.Point(20, 60);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(235, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Отменить";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(184, 9);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(77, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(21, 9);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.ShowUpDown = true;
+            this.dateTimePicker2.Size = new System.Drawing.Size(66, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // infoButton
+            // 
+            this.infoButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.infoButton.Location = new System.Drawing.Point(93, 6);
+            this.infoButton.Name = "infoButton";
+            this.infoButton.Size = new System.Drawing.Size(85, 23);
+            this.infoButton.TabIndex = 4;
+            this.infoButton.Text = "Информация";
+            this.infoButton.UseVisualStyleBackColor = true;
+            this.infoButton.Click += new System.EventHandler(this.infoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(267, 86);
+            this.Controls.Add(this.infoButton);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnExec);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SDTC by tr1ckshxt";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button infoButton;
+    }
+}
+
---
+++ ShutDownComputer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ShutDownComputer
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+        static extern public int system(string command);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            var diff = dateTimePicker2.Value.TimeOfDay - dateTime.TimeOfDay;
+            diff += dateTimePicker1.Value.Date - dateTime.Date;
+            if (diff.TotalSeconds < 60)
+            {
+                MessageBox.Show("Вы не можете установить таймер\nменьше чем на 1 минуту!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var a = system($"shutdown -s -t {(int)diff.TotalSeconds}");
+                if (a != 0)
+                    MessageBox.Show("Таймер уже установлен\nОтмените его, если не устанавливали его раньше!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                    MessageBox.Show($"Таймер установлен, тамер будет выключен в {dateTimePicker1.Value.Date.Add(dateTimePicker2.Value.TimeOfDay)}", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            var a = system("shutdown -a");
+            if (a != 0)
+                MessageBox.Show("Таймер отсутствует", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+                MessageBox.Show("Таймер успешно отменен", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void infoButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Copyright © tr1ckshxt \"Shut down computer timer\" 2022 All rights reserved","", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
